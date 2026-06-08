2a665ef4893177c4c0ab7c43c595e05142556dd4
Cristian Machicado <cristian.machar@gmail.com>
2026-06-08 17:48:50 -0400

    actualize en encabezado

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -3,7 +3,7 @@
 
 @section('contenido')
     <section class="hero">
-        <h1>Bienvenido a mi proyecto final</h1>
+        <h1>Panel de control universitario</h1>
         <h2>Bienvenido a mi Portafolio Académico</h2>
         <p>Hola, soy Cristian Machicado, estudiante de Ingeniería de Sistemas.</p>
         <p><i>"La vida se construye línea por línea y precepto por precepto"</i></p>
