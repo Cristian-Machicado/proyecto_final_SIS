15b57f88082987e295ead162ea1ea4a820544ac3
Cristian Machicado <cristian.machar@gmail.com>
2026-06-08 17:35:32 -0400

    feat: mejorar estilos de la pagina de inicio

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('titulo', 'Inicio')
+
+@section('contenido')
+    <section class="hero">
+        <h1>Bienvenido a mi proyecto final</h1>
+        <h2>Bienvenido a mi Portafolio Académico</h2>
+        <p>Hola, soy Cristian Machicado, estudiante de Ingeniería de Sistemas.</p>
+        <p><i>"La vida se construye línea por línea y precepto por precepto"</i></p>
+    </section>
+@endsection
